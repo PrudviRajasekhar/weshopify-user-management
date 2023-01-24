78311335aab20ea8477e2c18c60b9507af4d42db
Narsi <nsanda@innominds.com>
2023-01-24 09:36:22 +0530

    added the platform commons

---
+++ weshopify-platform-commons/src/main/java/com/weshopify/platform/model/WSO2User.java
@@ -0,0 +1,38 @@
+package com.weshopify.platform.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+@Jacksonized
+@Data
+@Builder
+//@AllArgsConstructor(staticName = "of")
+public class WSO2User implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3877212295621231125L;
+	
+	private String[] schemas;
+	private String userName;
+	private String password;
+	private String[] emails;
+	
+	//@JsonIgnore
+	private String id;
+	
+	private WSO2UserPersonals name;
+	
+	//@JsonIgnore
+	private List<Role> roles;
+	private List<WSO2PhoneNumbers> phoneNumbers;
+
+}
